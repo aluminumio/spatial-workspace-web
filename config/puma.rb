049bc51275a269e923be0941ced0d7af156fbeb0
Jonathan Siegel <248302+usiegj00@users.noreply.github.com>
2026-02-16 21:09:40 +0900

    Fix Puma pidfile crash on PaaS (no tmp/pids dir)

--- config/puma.rb
+++ config/puma.rb
@@ -5,6 +5,4 @@ threads min_threads_count, max_threads_count
 port ENV.fetch("PORT", 3000)
 environment ENV.fetch("RAILS_ENV", "development")
 
-pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
-
-plugin :tmp_restart
+pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid") if Dir.exist?("tmp/pids")
